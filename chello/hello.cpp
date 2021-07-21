e516aaba86cd5add3d8e6ec7c344659f2a9c8780
l-czw <1659962583@qq.com>
2021-07-21 06:21:32 -0700

    3

---
+++ chello/hello.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Author: czw
+ * @Date: 2021-07-21 05:18:58
+ * @LastEditors: czw
+ * @LastEditTime: 2021-07-21 05:31:02
+ */
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=0;i<5;++i)
+        cout<<"czwjss!"<<endl;
+    return 0;
+} // namespace std;
+
